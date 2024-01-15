09f301bc5cbfe9fe47c016c83d7796136540b7f6
itonse <govl3dnjs@gmail.com>
2024-01-15 09:21:48 +0900

    refactor : @JobScope, @StepScope 어노테이션 추가
    Step 빈 등록시에는 @JobScope, 태스클릿 빈 등록시에는 @StepScope 필수

--- src/main/java/com/ll/springbatch/batch/HelloJobConfig.java
+++ src/main/java/com/ll/springbatch/batch/HelloJobConfig.java
@@ -1,8 +1,9 @@
-package com.ll.springbatch.config;
+package com.ll.springbatch.batch;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -24,6 +25,7 @@ public class HelloJobConfig {
                 .build();
     }
 
+    @JobScope
     @Bean
     public Step helloStep1(JobRepository jobRepository, Tasklet helloStep1Tasklet, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("helloStep1Tasklet", jobRepository)
@@ -31,6 +33,7 @@ public class HelloJobConfig {
                 .build();
     }
 
+    @JobScope
     @Bean
     public Tasklet helloStep1Tasklet() {
         return ((contribution, chunkContext) -> {
